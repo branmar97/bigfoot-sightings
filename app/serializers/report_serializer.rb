8dd7ac2283415fba4a3f651db1ed10b7c1700da3
branmar97 <brandonmar97@gmail.com>
2022-04-14 10:58:05 -0400

    add id attribute to report serializer

--- app/serializers/report_serializer.rb
+++ app/serializers/report_serializer.rb
@@ -1,4 +1,4 @@
 class ReportSerializer
   include JSONAPI::Serializer
-  attributes :case_num, :occurence, :city, :state, :vicinity, :conditions, :witnesses, :evidence, :account, :prints, :sounds, :additional_info, :created_at, :updated_at
+  attributes :id, :case_num, :occurence, :city, :state, :vicinity, :conditions, :witnesses, :evidence, :account, :prints, :sounds, :additional_info, :created_at, :updated_at
 end
