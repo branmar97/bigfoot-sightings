76e8f278338048aaab1f6293297cd2ff497aea8b
branmar97 <brandonmar97@gmail.com>
2022-04-14 11:04:48 -0400

    create reports index action

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,6 @@
 class ReportsController < ApplicationController
+    def index
+        @reports = Report.all
+        render json: ReportSerializer.new(reports).serializable_hash.to_json
+    end
 end
