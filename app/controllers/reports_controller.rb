4fea8fe7d09321b0e47c798886f641e2adc732ee
branmar97 <brandonmar97@gmail.com>
2022-04-14 11:18:02 -0400

    add set report method

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,4 +3,10 @@ class ReportsController < ApplicationController
         @reports = Report.all
         render json: ReportSerializer.new(reports).serializable_hash.to_json
     end
+
+    private 
+
+    def set_report
+        @report = Report.find(params[:id])
+    end 
 end
