b641777ae4c52aca2f5b661f8ec579591018821f
branmar97 <brandonmar97@gmail.com>
2022-04-15 09:53:08 -0400

    add create action to reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,6 +10,15 @@ class ReportsController < ApplicationController
         render json: ReportSerializer.new(@report).serializable_hash[:data][:attributes].to_json
     end
 
+    def create 
+        report = Report.new(report_params)
+        if report.save
+            render json: ReportSerializer.new(report).serializable_hash[:data][:attributes].to_json
+        else 
+            render json: report.errors, status: 400
+        end
+    end 
+
     private 
 
     def set_report
